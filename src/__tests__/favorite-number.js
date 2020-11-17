abbf4705bfc7705f20e3aabc2cad103312b04ed2
Paulo Henrique <paulo.scape@gmail.com>
2020-11-17 17:23:47 -0300

    :sparkles: add testing with the user-event module

--- src/__tests__/favorite-number.js
+++ src/__tests__/favorite-number.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import {screen, render, fireEvent} from '@testing-library/react'
+import user from '@testing-library/user-event'
 import {FavoriteNumber} from '../favorite-number'
 
 test('renders a number input with a label "Favorite Number"', () => {
@@ -10,7 +11,7 @@ test('renders a number input with a label "Favorite Number"', () => {
   // debug(input)
 })
 
-test('entering an invalid value shows an error message', () => {
+test('entering an invalid value shows an error message [fireEvent]', () => {
   render(<FavoriteNumber />)
   const input = screen.getByLabelText(/favorite number/i)
   fireEvent.change(input, {
@@ -18,5 +19,12 @@ test('entering an invalid value shows an error message', () => {
       value: '10',
     },
   })
+  expect(screen.getByRole('alert')).toHaveTextContent(/the number is invalid/i)
+})
+
+test('entering an invalid value shows an error message [user-event module]', () => {
+  render(<FavoriteNumber />)
+  const input = screen.getByLabelText(/favorite number/i)
+  user.type(input, '10')
   expect(screen.getByRole('alert')).toHaveTextContent(/the number is invalid/i)
 })
