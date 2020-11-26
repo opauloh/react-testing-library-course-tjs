f7ac1554b75a00cc154562c2a594dd676fab5afa
Paulo Henrique <paulo.scape@gmail.com>
2020-11-26 16:57:21 -0300

    :sparkles: Ensure Error Boundaries Can Successfully Recover from Errors

--- src/__tests__/error-boundary.js
+++ src/__tests__/error-boundary.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {render} from '@testing-library/react'
+import {fireEvent, render, screen} from '@testing-library/react'
 import {reportError as mockReportError} from '../api'
 import {ErrorBoundary} from '../error-boundary'
 
@@ -44,4 +44,47 @@ test('calls reportError and renders that where was a problem', () => {
   expect(mockReportError).toHaveBeenCalledWith(error, info)
   expect(mockReportError).toHaveBeenCalledTimes(1)
   expect(console.error).toHaveBeenCalledTimes(2) // twice, once by jest-dom and once by react-dom
+})
+
+test('Ensure Error Boundaries Can Successfully Recover from Errors', () => {
+  mockReportError.mockResolvedValueOnce({success: true})
+
+  const {rerender} = render(
+    <ErrorBoundary>
+      <Bomb />
+    </ErrorBoundary>,
+  )
+
+  rerender(
+    <ErrorBoundary>
+      <Bomb shouldThrow />
+    </ErrorBoundary>,
+  )
+
+  const error = expect.any(Error)
+  const info = {componentStack: expect.stringContaining('Bomb')}
+  expect(mockReportError).toHaveBeenCalledWith(error, info)
+  expect(mockReportError).toHaveBeenCalledTimes(1)
+  expect(console.error).toHaveBeenCalledTimes(2) // twice, once by jest-dom and once by react-dom
+
+  expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
+    `"There was a problem."`,
+  )
+
+  console.error.mockClear()
+  mockReportError.mockClear()
+
+  rerender(
+    <ErrorBoundary>
+      <Bomb />
+    </ErrorBoundary>,
+  )
+
+  fireEvent.click(screen.getByText(/try again/i))
+
+  expect(mockReportError).not.toHaveBeenCalled()
+  expect(console.error).not.toHaveBeenCalled()
+
+  expect(screen.queryByRole('alert')).not.toBeInTheDocument()
+  expect(screen.queryByText(/try again/i)).not.toBeInTheDocument()
 })
