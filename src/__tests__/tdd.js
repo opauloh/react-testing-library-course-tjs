3b62ce2e7ff86279c3a0de87bf79b011efc5145a
Paulo Henrique <paulo.scape@gmail.com>
2020-11-27 10:06:12 -0300

    :white_check_mark: tdd - test form submission

--- src/__tests__/tdd.js
+++ src/__tests__/tdd.js
@@ -1,9 +1,15 @@
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import {fireEvent, render, screen} from '@testing-library/react'
 
 function Editor() {
+  const [isSaving, setIsSaving] = React.useState(false)
+  const handleSubmit = e => {
+    e.preventDefault()
+    setIsSaving(true)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="title-input">Title</label>
       <input id="title-input" />
 
@@ -13,7 +19,9 @@ function Editor() {
       <label htmlFor="tags-input">Tags</label>
       <input id="tags-input" />
 
-      <button type="submit">Submit</button>
+      <button type="submit" disabled={isSaving}>
+        Submit
+      </button>
     </form>
   )
 }
@@ -24,4 +32,13 @@ test('renders a form with title, content, tags and a submit button', () => {
   expect(screen.getByLabelText(/content/i)).toBeInTheDocument()
   expect(screen.getByLabelText(/tags/i)).toBeInTheDocument()
   expect(screen.getByText(/submit/i)).toBeInTheDocument()
+})
+
+test('button is disabled after click submit', () => {
+  render(<Editor />)
+  const submitButton = screen.getByText(/submit/i)
+
+  fireEvent.click(submitButton)
+
+  expect(submitButton).toBeDisabled()
 })
