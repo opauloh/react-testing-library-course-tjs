4744b8e93591e4d34a5732cd24b0b11a3528265c
Paulo Henrique <paulo.scape@gmail.com>
2020-11-27 10:33:37 -0300

    :white_check_mark: tdd test api call on submit

--- src/__tests__/tdd.js
+++ src/__tests__/tdd.js
@@ -1,23 +1,34 @@
 import * as React from 'react'
 import {fireEvent, render, screen} from '@testing-library/react'
+import {savePost, savePost as mockSavePost} from '../api'
 
-function Editor() {
+function Editor({user}) {
   const [isSaving, setIsSaving] = React.useState(false)
   const handleSubmit = e => {
     e.preventDefault()
+
+    const {title, content, tags} = e.target.elements
+    const newPost = {
+      title: title.value,
+      content: content.value,
+      tags: tags.value.split(',').map(t => t.trim()),
+      authorId: user.id,
+    }
+
     setIsSaving(true)
+    savePost(newPost)
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="title-input">Title</label>
-      <input id="title-input" />
+      <input id="title-input" name="title" />
 
       <label htmlFor="content-input">Content</label>
-      <textarea id="content-input" />
+      <textarea id="content-input" name="content" />
 
       <label htmlFor="tags-input">Tags</label>
-      <input id="tags-input" />
+      <input id="tags-input" name="tags" />
 
       <button type="submit" disabled={isSaving}>
         Submit
@@ -26,19 +37,38 @@ function Editor() {
   )
 }
 
-test('renders a form with title, content, tags and a submit button', () => {
-  render(<Editor />)
-  expect(screen.getByLabelText(/title/i)).toBeInTheDocument()
-  expect(screen.getByLabelText(/content/i)).toBeInTheDocument()
-  expect(screen.getByLabelText(/tags/i)).toBeInTheDocument()
-  expect(screen.getByText(/submit/i)).toBeInTheDocument()
+jest.mock('../api')
+
+afterEach(() => {
+  jest.clearAllMocks()
 })
 
-test('button is disabled after click submit', () => {
-  render(<Editor />)
+test('renders a form with title, content, tags and a submit button', () => {
+  const fakePost = {
+    title: 'Test Title',
+    content: 'Test Content',
+    tags: ['tag1', 'tag2'],
+  }
+  const fakeUser = {
+    id: 'user-1',
+  }
+
+  render(<Editor user={fakeUser} />)
+
+  screen.getByLabelText(/title/i).value = fakePost.title
+  screen.getByLabelText(/content/i).value = fakePost.content
+  screen.getByLabelText(/tags/i).value = fakePost.tags.join(', ')
+
   const submitButton = screen.getByText(/submit/i)
 
   fireEvent.click(submitButton)
 
   expect(submitButton).toBeDisabled()
+
+  expect(mockSavePost).toHaveBeenCalledWith({
+    ...fakePost,
+    authorId: fakeUser.id,
+  })
+
+  expect(mockSavePost).toHaveBeenCalledTimes(1)
 })
