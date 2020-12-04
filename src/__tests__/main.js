e7839228218057bbb15f3ce35253b5e65e06e012
Paulo Henrique <paulo.scape@gmail.com>
2020-12-04 10:19:51 -0300

    :sparkles: add custom render

--- src/__tests__/main.js
+++ src/__tests__/main.js
@@ -1,13 +1,21 @@
 import * as React from 'react'
 import {BrowserRouter} from 'react-router-dom'
-import {render, screen} from '@testing-library/react'
+import {render as rtlRender, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {Main} from '../main'
 
-test('main renders about and home and We can navigate between those pages', () => {
-  window.history.pushState({}, 'Test page', '/')
+function render(ui, {route = '/', ...renderOptions} = {}) {
+  window.history.pushState({}, 'Test page', [route])
+  function Wrapper({children}) {
+    return <BrowserRouter>{children}</BrowserRouter>
+  }
+  return rtlRender(ui, {wrapper: Wrapper, ...renderOptions})
+}
 
-  render(
+test('main renders about and home and We can navigate between those pages', () => {
+  window.history.pushState({}, 'Test page', ['/'])
+
+  rtlRender(
     <BrowserRouter>
       <Main />
     </BrowserRouter>,
@@ -21,13 +29,19 @@ test('main renders about and home and We can navigate between those pages', () =
 })
 
 test('landing on a bad page shows no match component', () => {
-  window.history.pushState({}, 'Test page', '/something-that-does-not-match')
-
-  render(
-    <BrowserRouter>
-      <Main />
-    </BrowserRouter>,
-  )
+  render(<Main />, {
+    route: '/something-that-does-not-match',
+  })
 
   expect(screen.getByRole('heading')).toHaveTextContent(/404/i)
+})
+
+test('main renders about and home and We can navigate between those pages [custom render]', () => {
+  render(<Main />)
+
+  expect(screen.getByRole('heading')).toHaveTextContent(/home/i)
+
+  userEvent.click(screen.getByText(/about/i))
+
+  expect(screen.getByRole('heading')).toHaveTextContent(/about/i)
 })
