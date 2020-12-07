c336222d6e21720f9b6df945a71d80f6142ae58a
Paulo Henrique <paulo.scape@gmail.com>
2020-12-07 10:48:07 -0300

    :sparkles: add custom render to our redux provider

--- src/__tests__/redux-counter.js
+++ src/__tests__/redux-counter.js
@@ -1,28 +1,35 @@
 import * as React from 'react'
-import {fireEvent, render, screen} from '@testing-library/react'
+import {fireEvent, render as rtlRender, screen} from '@testing-library/react'
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
 import {Counter} from '../redux-counter'
-import {store as appStore} from '../redux-store'
 import {reducer} from '../redux-reducer'
 
+function render(
+  ui,
+  {
+    initialState,
+    store = createStore(reducer, initialState),
+    ...rtlOptions
+  } = {},
+) {
+  function Wrapper({children}) {
+    return <Provider store={store}>{children}</Provider>
+  }
+  return {
+    ...rtlRender(ui, {wrapper: Wrapper, ...rtlOptions}),
+    store,
+  }
+}
+
 test('can render with redux with defaults', () => {
-  render(
-    <Provider store={appStore}>
-      <Counter />
-    </Provider>,
-  )
+  render(<Counter />)
   fireEvent.click(screen.getByText('+'))
   expect(screen.getByLabelText(/count/i)).toHaveTextContent('1')
 })
 
-test('can render with redu with custom initial state', () => {
-  const store = createStore(reducer, {count: 3})
-  render(
-    <Provider store={store}>
-      <Counter />
-    </Provider>,
-  )
+test('can render with redux with custom initial state', () => {
+  render(<Counter />, {initialState: {count: 3}})
   fireEvent.click(screen.getByText('-'))
   expect(screen.getByLabelText(/count/i)).toHaveTextContent('2')
 })
