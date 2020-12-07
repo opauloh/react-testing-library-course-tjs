b8fe9cb6726715bb3e65be2aa6edb46d107a8d7c
Paulo Henrique <paulo.scape@gmail.com>
2020-12-07 10:34:31 -0300

    :sparkles: add initialized redux example

--- src/__tests__/redux-counter.js
+++ src/__tests__/redux-counter.js
@@ -1,15 +1,28 @@
 import * as React from 'react'
 import {fireEvent, render, screen} from '@testing-library/react'
 import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 import {Counter} from '../redux-counter'
-import {store} from '../redux-store'
+import {store as appStore} from '../redux-store'
+import {reducer} from '../redux-reducer'
 
 test('can render with redux with defaults', () => {
   render(
-    <Provider store={store}>
+    <Provider store={appStore}>
       <Counter />
     </Provider>,
   )
   fireEvent.click(screen.getByText('+'))
   expect(screen.getByLabelText(/count/i)).toHaveTextContent('1')
+})
+
+test('can render with redu with custom initial state', () => {
+  const store = createStore(reducer, {count: 3})
+  render(
+    <Provider store={store}>
+      <Counter />
+    </Provider>,
+  )
+  fireEvent.click(screen.getByText('-'))
+  expect(screen.getByLabelText(/count/i)).toHaveTextContent('2')
 })
